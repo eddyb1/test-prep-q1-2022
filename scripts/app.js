1221210de733b5a41648e9602c96ec05a645334b
Braeden <eddybm@s.dcsdk12.org>
2022-10-12 14:26:31 -0600

    10/12/22

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,26 @@
 //@ts-check
 
 // Decalare a variable called "name" and assign your first name to it.
-
+let name = "Braeden"
 // Log the variable you just created out to the console
+console.log;(name ="Braeden")                 
 
 // change the following code so that "watermelon" is written to the console
-let fruit = "";
-console.log("The fruit variable equals: ", fruit);
+let fruit = "watermelon";
+console.log(fruit=watermelon), fruit;
 
 // Change the following code so the array is filtered to your
 // favorite fruit.  Feel free to add to the fruits array
 // if your favorite isn't in there.
 let fruits = ["grape", "apple", "kiwi", "strawberry"];
-fruits = fruits.filter((f) => "");
-console.log("The array of fruits now equals: ", fruits);
-
+fruits = fruits.filter((f) => "watermelon");
+console.log("The array of fruits now equals: "watermelon,' fruits)
 // Create a new class called Player. Have the Player class
 // take in x and y coordinates as input parameters to it's
 // constructor.  Setup a property on the class named color,
 // and initialize the color property to a string value (you
 // can use a basic color or HSL string, your choice).
-//
+
 // Create a method on the class named update.  When update
 // is called, increment the x and y coordinates by 1.
 //
